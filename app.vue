0dc36937b9e145113f6d7958aaa2f06eac127068
josh-eebz <113428744+josh-eebz@users.noreply.github.com>
2023-07-27 15:46:50 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  Dynamic Import
+  <!-- Not working -->
+  <br />
+  <component :is="GroupTest" />
+  <br />
+  Manual Import
+  <br />
+  <!-- Working -->
+  <grouptest />
+  <br />
+  Nuxt Import
+  <br />
+  <GroupTest />
+</template>
+
+<script>
+import grouptest from '@/components/group/test';
+
+export default {
+  components: {
+    grouptest,
+  },
+};
+</script>
